e93e4d0b9bf8b2bb358b1d5906a688d15c184a97
javython999 <infinity2544@gmail.com>
2025-08-21 20:46:11 +0900

    :hammer: add test code

---
+++ src/test/java/com/errday/kafkahelper/domain/KafkaTopicConfigDescribeTest.java
@@ -0,0 +1,11 @@
+package com.errday.kafkahelper.domain;
+
+import org.junit.jupiter.api.Test;
+
+class KafkaTopicConfigDescribeTest {
+
+    @Test
+    void of() {
+        KafkaBootStrapServer kafkaBootStrapServer = KafkaBootstrapServerFixture.mock();
+    }
+}
